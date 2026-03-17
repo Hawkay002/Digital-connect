21e9c5abcd1a50082278ad122da58512b908500d
Shovith Debnath <119169431+Hawkay002@users.noreply.github.com>
2026-03-17 20:08:13 +0530

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from 'react';
-import { db, auth, messaging } from '../firebase'; 
+import { db, auth, messaging } from '../firebase';  
 import { collection, query, where, getDocs, getDoc, doc, deleteDoc, setDoc, onSnapshot, updateDoc, addDoc } from 'firebase/firestore'; 
 import { getToken } from 'firebase/messaging'; 
 import { Link, useNavigate } from 'react-router-dom';
